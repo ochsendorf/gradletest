7bdc17a8d478a49225ed18e0a152f3e78d54bdd8
thiago <thiago.ochsendorf@gmail.com>
2017-08-27 10:26:47 -0300

    uploading project

---
+++ src/main/java/adt/web/Bello.java
@@ -0,0 +1,10 @@
+package adt.web;
+
+public class Bello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/gr/Hallo.java
@@ -0,0 +1,10 @@
+package gr;
+
+public class Hallo {
+
+	public static void main(String[] args) {
+		System.out.println("Hallo");
+
+	}
+
+}
